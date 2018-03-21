e62961df96b7ac46a47b2ae420e3c0fc4491ac88
ismael1987 <ismael1987@example.com>
2018-03-21 19:10:09 +0100

    tset app

---
+++ src/main/java/at/problemtest/problemtest/ProblemtestApplication.java
@@ -0,0 +1,12 @@
+package at.problemtest.problemtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemtestApplication.class, args);
+	}
+}
---
+++ src/test/java/at/problemtest/problemtest/ProblemtestApplicationTests.java
@@ -0,0 +1,16 @@
+package at.problemtest.problemtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProblemtestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
